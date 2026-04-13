e79c47656e8baeb55fe7da78630eeb9d92a5b0b9
ayankhan-coder <ayankhan786490@gmail.com>
2026-04-13 14:21:10 +0530

    loops

---
+++ src/loops.java
@@ -0,0 +1,71 @@
+import com.sun.security.jgss.GSSUtil;
+
+public class loops {
+    public static void main(String[] args) {
+        /*int num = 5;
+        for (int i=0; i<num; i++ ){
+            System.out.println(2*i+1);
+        }
+        System.out.println(",,,,,,,,,......");
+        int i = 1;  // counter
+        int numm = 1; // first odd number
+
+        while (i <= 5) {
+            System.out.println(numm);
+            numm = numm + 2; // next odd number
+            i++;
+        }
+        System.out.println("/***************************+");
+        for (int ii = 0; ii <= 10; ii++) {
+            System.out.println(2 * ii + 1);
+        }
+        int a = 5;
+        while (a>=1) {
+            System.out.println(a);
+            --a;
+        }
+        int i = 0;
+        while (i<=5){
+            System.out.println(i);
+            if (i==2){
+                break;
+
+            }i++;
+
+        }
+        int i = 0;
+        do {
+            System.out.println(i);
+
+            if (i==2){
+                break;
+            }
+            i++;
+        }while (i<=5);
+        for (int i=0; i<=10; i++){
+            if (i==5){
+                continue;
+            }
+            System.out.println(i);
+
+          }
+       int  i = 0;
+       do {
+           i++;
+           if (i==2){
+               continue;
+           }
+           System.out.println(i);
+
+       }while (i<=10);*/
+        int i = 0;
+        while (i<=10){
+            i++;
+            if (i==2){
+                continue;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
