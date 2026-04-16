44f6dcb28cf1d6156d972bb0510bff5aa2435c5c
ayankhan-coder <ayankhan786490@gmail.com>
2026-04-16 07:45:11 +0530

    initial commit

--- src/This_Super_KeyWord.java
+++ src/This_Super_KeyWord.java
@@ -1,6 +1,6 @@
 /* THIS & SUPER keyWord in Java */
 
-class Person {
+class Person {                  // L1----------
     String name;
 
     Person(String name) {
@@ -26,9 +26,42 @@ class Student extends Person {
     }
 }
 
+class Vehicle {
+    String brand;
+    public Vehicle(String brand) {
+        this.brand = brand;
+    }
+    public String getBrand() {
+        return brand;
+    }
+}
+
+/*class Car extends Vehicle {
+    int price;
+    String fuelType;
+
+    public Car(String brand, int price) {
+        super(brand);
+        this.price = price;
+        this.fuelType = "Petrol";   //  default
+    }
+    public Car(String brand, int price, String fuelType) {
+        super(brand);
+        this.price = price;
+        this.fuelType = fuelType;
+    }
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Price: " + price);
+        System.out.println("Fuel: " + fuelType);
+    }
+}*/
+//########################################################################################
+//########################################################################################
 class This_Super_keyWord{
     public static void main(String[] args) {
-        Student student = new Student("Ayan");
+
+        Student student = new Student("Ayan");        // L1-----------
         Student s = new Student("Ayan","java");
 
         System.out.println(student.getName());
@@ -38,10 +71,11 @@ class This_Super_keyWord{
         System.out.println(s.getCourse());
 
 
+       /* Car c1 = new Car("Toyota", 500000);           // L2----------
+        Car c2 = new Car("Mahindra", 900000, "Diesel");
 
-
-
-
+        c1.displayDetails();
+        c2.displayDetails();*/
 
     }
 }
