5088f50937ef3eda04e6a81d8a14d4d1c5fe7e94
ayankhan-coder <ayankhan786490@gmail.com>
2026-05-19 20:07:07 +0530

    initial commit

--- src/practice/Errors_Exceptions.java
+++ src/practice/Errors_Exceptions.java
@@ -3,22 +3,21 @@ package practice;
 public class Errors_Exceptions {
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
-//        // Syntax Error Demo
-//
-////        int a = 2
-////        System.out.println(a);
-//
-//        // Logical Error Demo
-//        System.out.println(2);//Arithmetic error
-//        for (int i=1; i<5; i++){
-//            System.out.println(2*i+1);
-//        }
-//
-//        //Runtime Error or Exception Demo
-//        java.util.Scanner sc = new java.util.Scanner(System.in);
-//        System.out.print("Enter number to divide :");
-//        int num = sc.nextInt();//exception comes if user enter 0
-//        System.out.println(1000/num);
+        // Syntax Error Demo
+
+//        int a = 2
+//        System.out.println(a);
+
+        // Logical Error Demo
+        System.out.println(2);//Arithmetic error
+        for (int i=1; i<5; i++){
+            System.out.println(2*i+1);
+        }
+
+        //Runtime Error or Exception Demo
+        System.out.print("Enter number to divide :");
+        int num1 = sc.nextInt();//exception comes if user enter 0
+        System.out.println(1000/num1);
 
         /*~ Different Types of Exceptions Handling ~*/
 
@@ -78,25 +77,5 @@ public class Errors_Exceptions {
             }
         }
         System.out.println("Program Ended");*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
