0d13118c9e305a491bbe34129b4ba2b6ba19956c
ayankhan-coder <ayankhan786490@gmail.com>
2026-06-11 07:23:26 +0530

    hashMap added

---
+++ src/practice/HashSet_HashMap.java
@@ -0,0 +1,41 @@
+package practice;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class HashSet_HashMap {
+    public static void main(String[] args) {
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(null);
+        set.remove(10);
+        System.out.println(set);
+        System.out.println("=================================");
+
+        HashMap<Integer , String> students = new HashMap<>();
+        students.put(101,"khan");
+        students.put(102,"Suresh");
+        students.put(103,"ramesh");
+        System.out.println(students);
+        System.out.println(students.get(102));
+        students.remove(103);
+        System.out.println(students.containsKey(101));
+        System.out.println(students.containsValue("Khan"));
+        System.out.println(students.get(101));
+
+        System.out.println(students.keySet());
+        System.out.println(students.values());
+        for (Map.Entry<Integer,String> e : students.entrySet()){
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+            System.out.println(students.entrySet());
+        System.out.println(students);
+
+    }
+}
