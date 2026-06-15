d1415ef77b955829dcfbb4db284583cde428809a
ayankhan-coder <ayankhan786490@gmail.com>
2026-06-15 07:04:41 +0530

    ArrayDeque

--- src/collectionFrameworks/ArrayDeque_.java
+++ src/collectionFrameworks/ArrayDeque_.java
@@ -1,12 +1,21 @@
 package collectionFrameworks;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class ArrayDeque_ {
     public static void main(String[] args) {
+
         /*Array Deque*/
-        java.util.ArrayDeque<Integer> ad = new java.util.ArrayDeque<>();
+        Deque<Integer> ad = new ArrayDeque<>();
         ad.add(10);
         ad.add(30);
+        ad.addFirst(50);
+        ad.addLast(60);
         ad.removeLast();
+        ad.removeFirst();
         System.out.println(ad.peekFirst());
+        for (int element : ad){
+            System.out.println(element);
+        }
     }
 }
